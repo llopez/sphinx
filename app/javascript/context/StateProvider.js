2e4b4af32107c75c48dd56255d1bf9a149d97021
Luis Lopez <luis.lopez@altoros.com>
2021-02-15 15:25:21 -0300

    add some token images, fake data

--- app/javascript/context/StateProvider.js
+++ app/javascript/context/StateProvider.js
@@ -33,6 +33,33 @@ const initialState = {
       erc20s: [
         { symbol: 'dai', balance: 1000 },
       ]
+    },
+    {
+      label: 'Ledger #4',
+      address: '0x975787a4CF92e46c5f529b08da00AD90B0C35e92',
+      ether: 2.3,
+      total: 1000.34,
+      erc20s: [
+        { symbol: 'dai', balance: 1000 },
+      ]
+    },
+    {
+      label: 'Other',
+      address: '0x0Bb22b99916B487B6A2699cD265b7bF2e986AFB0',
+      ether: 2.3,
+      total: 1000.34,
+      erc20s: [
+        { symbol: 'dai', balance: 1000 },
+      ]
+    },
+    {
+      label: 'Meta 1',
+      address: '0x5aBB4561db04a0ddf6cdaE0c2369536309A8aC48',
+      ether: 2.3,
+      total: 1000.34,
+      erc20s: [
+        { symbol: 'dai', balance: 1000 },
+      ]
     }
   ],
   tokens: [
