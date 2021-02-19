f3b34a134358ae899be4c740b158775fdf7e29ba
Luis Lopez <luis.lopez@altoros.com>
2021-02-19 17:23:25 -0300

    wip

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,7 @@
 class PagesController < ApplicationController
+  protect_from_forgery except: :service_worker
+
+  def service_worker
+    render layout: false
+  end
 end
--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -6,6 +6,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from '../components/App'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('/service-worker.js', { scope: '/' }).then(function (registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function (err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <App />,
