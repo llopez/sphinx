84466d350155e9fc304edfd83a182d97d493770d
Luis Lopez <luis.lopez@altoros.com>
2021-03-21 19:48:01 -0300

    pull to refresh for rates

--- app/javascript/components/Market.jsx
+++ app/javascript/components/Market.jsx
@@ -2,23 +2,35 @@ import { CardContent, Card, Typography } from '@material-ui/core'
 import React, { useEffect, useContext } from 'react'
 import TokensList from './market/TokensList'
 import Context from '../context/Context'
+import PullToRefresh from 'react-simple-pull-to-refresh'
 
 const Market = () => {
   const [{ tokens }, dispatch] = useContext(Context)
 
-  useEffect(() => {
-    fetch('/api/rates')
+  const fetchRates = () => {
+    return fetch('/api/rates')
       .then(res => res.json())
       .then(data => dispatch({ type: 'LOAD_TOKENS', payload: data }))
+  }
+
+  const handlePull = () => {
+    return fetchRates()
+  }
+
+  useEffect(() => {
+    fetchRates()
   }, [])
 
   return (
-    <Card>
-      <CardContent>
-        <Typography>Market</Typography>
-        <TokensList tokens={tokens} />
-      </CardContent>
-    </Card>)
+    <PullToRefresh onRefresh={handlePull}>
+      <Card>
+        <CardContent>
+          <Typography>Market</Typography>
+          <TokensList tokens={tokens} />
+        </CardContent>
+      </Card>
+    </PullToRefresh>
+  )
 }
 
 export default Market
